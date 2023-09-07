5d9a3b263b7fe7883486379c2e13c47a428a648f
John Sturgeon <john.sturgeon@me.com>
2023-09-07 14:05:17 -0700

    went from rocketry to APSchedule

--- scripts/jobs.py
+++ scripts/jobs.py
@@ -1,20 +1,22 @@
 """ Schedule that will load and run whatever is included in 'jobs' """
-
 import os
 import logging
-from rocketry import Rocketry
+from apscheduler.schedulers.blocking import BlockingScheduler
 
+SCHEDULE = os.getenv('SCHEDULE')
+TZ = os.getenv('TZ')
 
 logging.basicConfig(level=logging.INFO)
 
-app = Rocketry()
-SCHEDULE = os.getenv('SCHEDULE')
+scheduler = BlockingScheduler()
+scheduler.configure(timezone=TZ)
 
 
-@app.task(SCHEDULE)
 def do_something():
     """ Doing something"""
     logging.info("BING")
 
+
 if __name__ == '__main__':
-    app.run()
+    scheduler.add_job(do_something, 'interval', seconds=5)
+    scheduler.start()
