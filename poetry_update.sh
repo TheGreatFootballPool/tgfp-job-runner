a6e48ba1363bd32b27317aab5e3a8e0ab9d9dd13
John Sturgeon <john.sturgeon@me.com>
2023-12-27 07:31:12 -0800

    updated libraries

---
+++ poetry_update.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+poetry add tgfp-lib@latest
+poetry add tgfp-nfl@latest
+poetry add arrow@latest
+poetry add pylint@latest
+poetry add setuptools@latest
+poetry add prefect@latest
+poetry add discord@latest
+poetry add aiomqtt@latest
+poetry add matplotlib@latest
+poetry add beautifulsoup4@latest
+poetry add numpy@latest
+poetry add pillow@latest
